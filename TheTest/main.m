7084496a479582c3812cea95ead77e257513a538
fblife-zhang <zhangshaonan@fblife.com>
2014-03-03 11:40:13 +0800

    hello
    hello world

---
+++ TheTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheTest
+//
+//  Created by soulnear on 14-3-3.
+//  Copyright (c) 2014年 soulnear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
