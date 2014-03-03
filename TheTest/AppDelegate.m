70556f899f36c7df3814190aa9866896870b18fc
fblife-zhang <zhangshaonan@fblife.com>
2014-03-03 11:42:39 +0800

    my first commit
    my first commit

--- TheTest/AppDelegate.m
+++ TheTest/AppDelegate.m
@@ -17,6 +17,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    //my first push
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
